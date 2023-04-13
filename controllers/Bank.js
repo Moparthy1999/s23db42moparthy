f19e740b9d17897cc5c28e3f6392db12ff657d71
Moparthy1999 <bhargavkrishna.m1999@gmail.com>
2023-04-12 22:07:51 -0500

    Added POST

--- controllers/Bank.js
+++ controllers/Bank.js
@@ -15,9 +15,25 @@ exports.bank_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Bank detail: ' + req.params.id);
 };
 // Handle Bank create on POST.
-exports.bank_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Bank create POST');
-};
+exports.bank_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Bank();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.Bank_Name = req.body.Bank_Name;
+    document.Bank_Branch = req.body.Bank_Branch;
+    document.Bank_Code = req.body.Bank_Code;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    }
 // Handle Bank delete form on DELETE.
 exports.bank_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Bank delete DELETE ' + req.params.id);
