6fb96b5cb740571ae273d643f87388b4de863195
Moparthy1999 <bhargavkrishna.m1999@gmail.com>
2023-04-20 00:21:54 -0500

    Created new files and updated my Bank server page

--- controllers/Bank.js
+++ controllers/Bank.js
@@ -11,9 +11,17 @@ exports.bank_list = async function(req, res) {
     }
     };
 // for a specific Bank.
-exports.bank_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Bank detail: ' + req.params.id);
-};
+    exports.bank_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Bank.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle Bank create on POST.
 exports.bank_create_post = async function(req, res) {
     console.log(req.body)
@@ -35,12 +43,37 @@ exports.bank_create_post = async function(req, res) {
     }
     }
 // Handle Bank delete form on DELETE.
-exports.bank_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Bank delete DELETE ' + req.params.id);
-};
-// Handle Bank update form on PUT.
-exports.bank_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Bank update PUT' + req.params.id);
+exports.bank_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Bank.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    
+//Handle Bank update form on PUT.
+exports.bank_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Bank.findById( req.params.id)
+// Do updates of properties
+if(req.body.Bank_Name)
+toUpdate.Bank_Name = req.body.Bank_Name;
+if(req.body.Bank_Branch) toUpdate.Bank_Branch = req.body.Bank_Branch;
+if(req.body.Bank_Code) toUpdate.Bank_Code = req.body.Bank_Code;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 
 // VIEWS
@@ -55,3 +88,59 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 }
+// Handle a show one view with id specified by query
+exports.bank_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Bank.findById( req.query.id)
+    res.render('Bankdetail',
+    { title: 'Bank Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.bank_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('bankcreate', { title: 'Bank Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    }
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.bank_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Bank.findById(req.query.id)
+    res.render('bankupdate', { title: 'Bank Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    }
+
+    // Handle a delete one view with id from query
+    exports.bank_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Bank.findById(req.query.id)
+    res.render('bankdelete', { title: 'Bank Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    }
+
--- routes/Bank.js
+++ routes/Bank.js
@@ -3,4 +3,17 @@ const bank_controlers= require('../controllers/Bank');
 var router = express.Router();
 /* GET Bank */
 router.get('/', bank_controlers.bank_view_all_Page );
+
+/* GET detail Bank page */
+router.get('/detail', bank_controlers.bank_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', bank_controlers.bank_create_Page);
+
+/* GET create update page */
+router.get('/update', bank_controlers.bank_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', bank_controlers.bank_delete_Page);
+
 module.exports = router
