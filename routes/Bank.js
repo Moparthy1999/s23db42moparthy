3ae139fac3b5064e6276e2b2f3ec6a2120a8b54b
Moparthy1999 <bhargavkrishna.m1999@gmail.com>
2023-04-25 17:24:23 -0500

    Add secure to update

--- routes/Bank.js
+++ routes/Bank.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const bank_controlers= require('../controllers/Bank');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 /* GET Bank */
 router.get('/', bank_controlers.bank_view_all_Page );
 
@@ -11,7 +21,7 @@ router.get('/detail', bank_controlers.bank_view_one_Page);
 router.get('/create', bank_controlers.bank_create_Page);
 
 /* GET create update page */
-router.get('/update', bank_controlers.bank_update_Page);
+router.get('/update', secured, bank_controlers.bank_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', bank_controlers.bank_delete_Page);
